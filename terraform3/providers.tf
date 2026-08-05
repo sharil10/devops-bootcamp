528713232bd2763e7d3dd3343681ae1355d3f174
Sharil <aril9020@gmail.com>
2026-08-05 22:36:34 +0800

    add provider.tf to terraform3 directory

---
+++ terraform3/providers.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 1.15"
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.9"
+    }
+  }
+    backend "s3" {
+    bucket       = "bootcamp-2026-sharil"
+    key          = "terraform2/terraform.tfstate"
+    region       = "ap-southeast-1"
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+data "aws_caller_identity" "my_account" {}
