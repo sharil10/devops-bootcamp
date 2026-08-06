9a56eedb2d2f96a764c8da3cf1526b5dbc2a34fa
Mohamand Sharil bin Nadzimuddin Johari <113306854+sharil10@users.noreply.github.com>
2026-08-06 17:56:42 +0800

    terraform3 (#14)
    * terraform 3
    
    * add breakout room activity

---
+++ terraform3/outputs.tf
@@ -0,0 +1,15 @@
+output "server_ip_public" {
+  value = module.my_server_public.public_ip
+}
+
+output "ssm_command_public" {
+  value = "aws ssm start-session --target ${module.my_server_public.id}"
+}
+
+output "server_ip_private" {
+  value = module.my_server_private.public_ip
+}
+
+output "ssm_command_private" {
+  value = "aws ssm start-session --target ${module.my_server_private.id}"
+}
