62efa37cb9b6284040572be888de5820be716474
Mohamand Sharil bin Nadzimuddin Johari <113306854+sharil10@users.noreply.github.com>
2026-08-11 23:02:47 +0800

    ansible1 (#16)

---
+++ ansible1/ec2.tf
@@ -0,0 +1,50 @@
+data "aws_ami" "my_ami" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+}
+
+data "aws_iam_instance_profile" "my_ssm_profile" {
+  name = "EC2-SSM-Role"
+}
+
+data "aws_ssm_parameter" "token" {
+  name = "/devops-bootcamp-2026/tunnel-token"
+}
+
+module "node1" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 6.0"
+
+  name                   = "node1"
+  ami                    = data.aws_ami.my_ami.id
+  instance_type          = "t3.micro"
+  subnet_id              = module.my_vpc.public_subnets[0]
+  create_security_group  = false
+  vpc_security_group_ids = [module.my_sg.id]
+  key_name               = "sharil-keypair"
+   tags                   = { Name = "node1" }
+  
+
+  
+}
+
+module "node2" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 6.0"
+
+  name                   = "node2"
+  ami                    = data.aws_ami.my_ami.id
+  instance_type          = "t3.micro"
+  subnet_id              = module.my_vpc.public_subnets[0]
+  create_security_group  = false
+  vpc_security_group_ids = [module.my_sg.id]
+  key_name               = "sharil-keypair"
+   tags                   = { Name = "node2" }
+  
+}
+
